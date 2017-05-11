8374aea93fb1719f7cec7d862995e3d288fda57b
Conner Johnson <Conner.johnson33@gmail.com>
2017-05-11 16:46:57 -0400

    key values in migration

--- endangered_api/db/migrate/20170511190448_create_species.rb
+++ endangered_api/db/migrate/20170511190448_create_species.rb
@@ -1,6 +1,11 @@
 class CreateSpecies < ActiveRecord::Migration[5.0]
   def change
     create_table :species do |t|
+      t.string  "common_name"
+      t.string  "scientific_name"
+      t.string  "status"
+      t.string  "threat"
+      t.string  "img_url"
     end
   end
 end
---
+++ endangered_api/db/migrate/20170511204233_create_comments.rb
@@ -0,0 +1,7 @@
+class CreateComments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :comments do |t|
+      t.string  "content"
+    end
+  end
+end

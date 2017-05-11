dedb289f6e37fad9596f7ca6b31c1db7434a7845
Conner Johnson <Conner.johnson33@gmail.com>
2017-05-11 16:28:51 -0400

    cat_js(show index)

--- endangered_api/app/controllers/categories_controller.rb
+++ endangered_api/app/controllers/categories_controller.rb
@@ -8,6 +8,12 @@ def index
   end
 end
 
+def show
+  @category = Category.find(params[:id])
 
+  respond_to do |format|
+    format.json { render json: @category }
+  end
+end
 
 end
--- endangered_api/config/routes.rb
+++ endangered_api/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :categories do
     resources :species
   end
+  resources :species
 end

e5dd91a17ae2038651ab2c92715a2a32e2ceadca
Paul Christian-Brown <pchristianbrown@gmail.com>
2017-05-13 00:15:33 -0400

    More seed input to Grasshopper sparrow

--- endangered_api/db/seeds.rb
+++ endangered_api/db/seeds.rb
@@ -69,10 +69,48 @@ species= Species.create!([
 {common_name:"Indiana Bat", scientific_name:"Myotis sodalis", status:"Endangered", threat:"A significant threat is human disturbance at winter caves, which causes aroused bats to deplete energy reserves (Mohr 1972, Engel et al. 1976). Handling of hibernating bats by researchers may result in the bats moving to the use of less suitable hibernacula, causing additional loss of fat reserves (Jones 1971 in Engel et al. 1976).", img_url:"https://ecos.fws.gov/docs/species_images/doc4847.jpg", category:"mammals"},
 
 {common_name:"Shortnose Sturgeon", scientific_name:"Acipenser brevirostrum", status:"Endangered", threat:"	Although some shortnose sturgeon may be harvested incidental to Atlantic sturgeon in Canada, there are no legal fisheries or by-catch allowances in U.S. waters. Principal threats to the survival of this species are blockage of up- and downstream migrations at dams, habitat loss or degradation due to dams and dam operations, river channel dredging, and pollution.", img_url:"https://ecos.fws.gov/docs/species_images/doc3737.jpg", category:"fishes"},
+# Start of new species seeds
+{common_name:"Cheat Mountain salamander", scientific_name:"Plethodon nettingi", status:"Threatened", threat:"	The main threat to this species is habitat loss and degradation due to logging, mining, recreational development, and road construction. Competition with Plethodon cinereous might also be a limiting factor. However, these threats are all minimal and the population is probably stable.", img_url:"https://ecos.fws.gov/docs/species_images/doc1752.jpg", category:"reptiles"},
 
-{common_name:"XXX", scientific_name:"Plethodon nettingi", status:"XXX", threat:"	XXX", img_url:"XXX", category:"XXX"},
-{common_name:"XXX", scientific_name:"XXX", status:"XXX", threat:"	XXX", img_url:"XXX", category:"XXX"},
-{common_name:"XXX", scientific_name:"XXX", status:"XXX", threat:"	XXX", img_url:"XXX", category:"XXX"},
-{common_name:"XXX", scientific_name:"XXX", status:"XXX", threat:"	XXX", img_url:"XXX", category:"XXX"},
-{common_name:"XXX", scientific_name:"XXX", status:"XXX", threat:"	XXX", img_url:"XXX", category:"XXX"},
+{common_name:"Roseate tern", scientific_name:"Sterna dougallii dougallii", status:"Threatened", threat:"	Predation by rats, ferrets, red foxes and Peregrine Falcon (Falco peregrinus) occurs locally, and can have significant effects, including complete breeding failure at some Azores colonies (Avery et al. 1995). Natural predators can often take a great toll on localised colonies, particularly when terns are disturbed from the nest by other birds and humans.", img_url:"https://ecos.fws.gov/docs/species_images/doc4948.jpg", category:"birds"},
+
+{common_name:"Florida Leafwing Butterfly", scientific_name:"Anaea troglodyta floridalis", status:"Threatened", threat:"Loss of habitat", img_url:"http://butterfliesofamerica.com/images/Nymphalidae/Charaxinae/Anaea_troglodyta_floridalis/Anaea_troglodyta_floridalis_HOLOTYPE_M_of_Anaea_aidea_floridalis-USA-FLORIDA-Miami-Dade_Co.-Florida_City_10-XII-1936-AMNH-3-sm.jpg", category:"arthropod"},
+
+{common_name:"Miami Blue Butterfly", scientific_name:"Cyclargus (=hemiargus) thomasi bethunebakeri", status:"Endangered", threat:"	Loss of habitat", img_url:"https://ecos.fws.gov/docs/species_images/doc5110.jpg", category:"arthropod"},
+
+{common_name:"American crocodile", scientific_name:"Crocodylus acutus", status:"Endangered", threat:"This species was hunted and overexploited for its hides in the 1930s until it was protected in the 1970s, however, illegal hunting still occurs. It is also threatened by habitat degradation from coastal development, including destruction of nesting grounds and the destruction of mangrove swamps for shrimp aquaculture.", img_url:"https://ecos.fws.gov/docs/species_images/doc3726.jpg", category:"reptiles"},
+
+{common_name:"Okaloosa Darter", scientific_name:"Etheostoma okaloosae", status:"Threatened", threat:"Competitive interactions with introduced Etheostoma edwini may affect the distribution and abundance of E. okaloosae in some areas while in other areas habitat degradation may be more influential in the distributions of the two species (Burkhead et al. 1992); however, available evidence indicates that E. edwini is not negatively affecting E. okaloosae in most areas.
+", img_url:"https://ecos.fws.gov/docs/species_images/doc4879.jpg", category:"fishes"},
+
+{common_name:"Key Deer", scientific_name:"Odocoileus virginianus clavium", status:"Endangered", threat:"hunting", img_url:"https://ecos.fws.gov/docs/species_images/doc4914.jpg", category:"mammals"},
+
+{common_name:"West Indian Manatee", scientific_name:"Trichechus manatus", status:"Vulnerable", threat:"About half of adult mortality rangewide is attributable to human-related causes, primarily watercraft collisions (Ackerman et al. 1995, Deutsch et al. 2002). This is significant because the manatee population growth rate is highly sensitive to changes in adult survival rate (Eberhardt and O’Shea 1995, Marmontel et al. 1997, Runge et al. 2004). The future of the Florida manatee is also jeopardized by the predicted loss and deterioration of warm-water habitat, including retirement or deregulation of aging power plants and reduction in natural spring flows.", img_url:"https://ecos.fws.gov/docs/species_images/doc3661.jpg", category:"mammals"},
+
+{common_name:"Anastasia Island Beach Mouse", scientific_name:"Peromyscus polionotus phasma", status:"Endangered", threat:"Predation", img_url:"https://ecos.fws.gov/docs/species_images/doc1970.jpg", category:"mammals"},
+
+{common_name:"Perdido Key Beach Mouse", scientific_name:"Peromyscus polionotus trissyllepsis", status:"Endangered", threat:"Predation", img_url:"https://ecos.fws.gov/docs/species_images/doc4484.bmp", category:"mammals"},
+
+{common_name:"Southeastern Beach Mouse", scientific_name:"Perromyscus pollionotus niveiventris", status:"Threatened", threat:"Predation", img_url:"https://ecos.fws.gov/docs/species_images/doc1971.jpg", category:"mammals"},
+
+{common_name:"Florida Panther", scientific_name:"Puma (=felis) concolor coryi", status:"Endangered", threat:"Human Predation and habitat loss", img_url:"https://ecos.fws.gov/docs/species_images/doc3663.jpg", category:"mammals"},
+
+{common_name:"Lower Keys Marsh Rabbit", scientific_name:"Sylvilagus palustris hefneri", status:"Endangered", threat:"Predation and habitat loss", img_url:"https://ecos.fws.gov/docs/species_images/doc5036.jpg", category:"mammals"},
+
+{common_name:"Frosted Flatwoods salamander", scientific_name:"Ambystoma cingulatum", status:"XXX", threat:"Potential threats include conversion of pine flatwoods habitat for agriculture, silviculture, or commercial/residential development; drainage or enlargement (with subsequent introduction of predatory fishes) of breeding ponds; habitat alteration resulting from suppression of fire; mortality and collecting losses associated with crayfish harvest; and highway mortality during migration.
+", img_url:"http://calphotos.berkeley.edu/imgs/512x768/1111_1111/1111/4649.jpeg", category:"reptiles"},
+
+{common_name:"Smalltooth Sawfish", scientific_name:"Pristis pectinata", status:"Critically Endangered", threat:"The principal threats to Smalltooth Sawfish are from fishing; it was formerly targeted, but is now mostly taken incidentally in broad-spectrum fisheries (CITES 2007). The long toothed rostrum of sawfish makes them extremely vulnerable to entanglement in any sort of net gear, gillnetting and trawling in particular. ", img_url:"https://ecos.fws.gov/docs/species_images/doc1736.jpg", category:"fish"},
+
+{common_name:"Florida scrub-jay", scientific_name:"Aphelocoma coerulescens", status:"Vulnerable", threat:"Fire suppression in remaining habitat has been identified as a major continuing threat because it causes scrub to overgrow and pine density to increase, rendering habitat unsuitable. Declines on lands specifically protected for the species are largely the result of reduced habitat quality, caused by disrupted fire regimes (Breininger et al. 1999, 2006, 2009, 2010).", img_url:"https://ecos.fws.gov/docs/species_images/doc4851.jpg", category:"birds"},
+
+{common_name:"Green Sea Turtle", scientific_name:"Chelonia mydas", status:"Endangered", threat:"Hunting, poaching, egg harvesting, boat strikes, pollution, and fishermen nets", img_url:"https://ecos.fws.gov/docs/species_images/doc3724.jpg", category:"reptiles"},
+
+{common_name:"Sand skink", scientific_name:"Neoseps reynoldsi", status:"Threatened", threat:"This species is threatened by habitat destruction for commercial and residential development, agricultural (citrus) development (USFWS 1987), and phosphate mining. Conversion of habitat to citrus production is the primary threat (P. Moler pers. comm. 1995). Habitat degradation due to fire exclusion is another threat (USFWS 1998).", img_url:"https://ecos.fws.gov/docs/species_images/doc5037.jpg", category:"reptiles"},
+
+{common_name:"Eastern Indigo Snake", scientific_name:"Drymarchon corais couperi", status:"Threatened", threat:"Decline is attributed to loss of mature longleaf pine habitat (e.g., conversion to slash and sand pine plantation, urbanization, citrus, mining, etc.), commercial collecting for pet trade (now illegal and has declined), and former widespread gassing of tortoise burrows (to collect rattlesnakes) (USFWS 1998). ", img_url:"https://ecos.fws.gov/docs/species_images/doc3730.jpg", category:"reptiles"},
+
+{common_name:"Cape Sable Seaside sparrow", scientific_name:"Ammodramus maritimus mirabilis", status:"Endangered", threat:"Predation and habitat loss", img_url:"https://ecos.fws.gov/docs/species_images/doc5035.jpg", category:"birds"},
+
+{common_name:"Florida Grasshopper sparrow", scientific_name:"Ammodramus savannarum floridanus", status:"Endangered", threat:"Predation and habitat loss", img_url:"https://ecos.fws.gov/docs/species_images/doc5034.jpg", category:"birds"},
 ])

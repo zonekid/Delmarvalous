a491dc9785ca0b71de6e4802ca96ca4e40127501
Marcus Linn Miller Jr <marcusmiller220@gmail.com>
2017-05-15 17:00:21 -0400

    half css

--- endangered_api/app/controllers/species_controller.rb
+++ endangered_api/app/controllers/species_controller.rb
@@ -1,20 +1,20 @@
 class SpeciesController < ApplicationController
 
-def index
-  @species= Species.all
+    def index
+        @species= Species.all
 
-  respond_to do |format|
-    format.json { render json: @species }
-  end
-end
+        respond_to do |format|
+            format.json { render json: @species }
+        end
+    end
 
-def show
-  # @category = Category.find(params[:category_id])
-  @specie = Species.find(params[:id])
+    def show
+        # @category = Category.find(params[:category_id])
+        @specie = Species.find(params[:id])
 
-  respond_to do |format|
-    format.json { render json: @specie }
-  end
-end
+        respond_to do |format|
+            format.json { render json: @specie.to_json(include: :category) }
+        end
+    end
 
 end
